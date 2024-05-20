7db9e76eb96e0e9eee274f10c13eeaeed17e6b70
Martin Baláž <martin.balaz@trojsten.sk>
2024-05-20 21:28:11 +0000

    Microfix to show_npy h

--- py/show_npy.py
+++ py/show_npy.py
@@ -11,6 +11,7 @@ parser.add_argument('infile', type=argparse.FileType('rb'))
 parser.add_argument('width', type=int, default=0)
 parser.add_argument('height', type=int, default=0)
 parser.add_argument('--two-sloped', '-t', action='store_true')
+parser.add_argument('--title', '-T', action='store_true')
 parser.add_argument('--output', '-o', type=argparse.FileType('wb'))
 args = parser.parse_args()
 
@@ -25,13 +26,18 @@ else:
     cmap = 'grey'
     norm = colors.Normalize(vmin=0)
 
+plt.rcParams.update({'font.size': 28})
+plt.figure(figsize=(16, 12))
 
 image = plt.imshow(y, origin='lower', cmap=cmap, norm=norm, extent=[0, width, 0, height])
-plt.title(args.infile.name)
+
+if args.title:
+    plt.title(args.infile.name)
+
 plt.colorbar(image)
 
 if args.output:
     plt.tight_layout()
-    plt.savefig(args.output, pad_inches=-0.1)
+    plt.savefig(args.output, dpi=100, pad_inches=-0.1)
 else:
     plt.show()
